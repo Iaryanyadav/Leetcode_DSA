07178ee81629f1409a9e1bc3ece10968d405c06b
Aryan <139036073+Iaryanyadav@users.noreply.github.com>
2026-09-09 03:08:23 +0530

    Time: 27 ms (6.84%) | Memory: 19.4 MB (26.63%) - LeetSync

--- 1260-day-of-the-year/day-of-the-year.py
+++ 1260-day-of-the-year/day-of-the-year.py
@@ -1,7 +1,11 @@
-import datetime
-
 class Solution:
     def dayOfYear(self, date: str) -> int:
-        if len(date) != 10:
-            return -1
-        return int(datetime.datetime.strptime(date, "%Y-%m-%d").strftime("%j"))
+        year, month, day = map(int, date.split('-'))
+        
+        days_in_months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        
+        is_leap = (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+        if is_leap:
+            days_in_months[1] = 29 
+            
+        return sum(days_in_months[:month - 1]) + day
